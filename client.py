2902d3481d3e7a8b73c3625c410b332ffdd3fa09
Mussabek Yerdaulet <53343530+myy04@users.noreply.github.com>
2025-07-14 14:16:38 +0800

    Add files via upload

---
+++ client.py
@@ -0,0 +1,277 @@
+
+import socket
+import threading
+import os
+import sys
+
+CHUNK_SIZE = 100
+MESSAGE_SIZE = 256
+MY_PEER_NAME = "NO_NAME"
+
+def getPeerList():
+    settings_file = open("../peer_settings.txt", "r")
+    peer_list = {}
+
+    for line in settings_file.readlines():
+        peer_info = line.split()
+        peer_list.update({peer_info[0] : (peer_info[1], int(peer_info[2]))})
+    
+    settings_file.close()
+    return peer_list
+
+def getFileList():
+    file_names = os.listdir("served_files")
+    files = [f for f in file_names if os.path.isfile(os.path.join("served_files", f)) and f.endswith(".txt")]
+    return files
+
+def getChunk(file_name, chunk_id):
+    directory = "served_files/" + file_name
+
+    text = ""
+    with open(directory, "r") as file:
+        text = file.read()
+
+    ret = ""
+    for i in range(chunk_id * CHUNK_SIZE, min(len(text), (chunk_id + 1) * CHUNK_SIZE)):
+        ret += text[i]
+
+    return ret
+
+def getBytesAmount(file_name):
+    return int(os.path.getsize('served_files/' + file_name))
+
+
+class ClientMain:
+    def __init__(self):    
+        self.downloading_files = {}
+
+    def requestFileList(self, peers):
+        for peer in peers:
+            t = ClientThread(peer, self)
+            t.requestFileList()
+
+    def requestFileDownload(self, peers, file_name):
+        if file_name in getFileList():
+            print(f"File {file_name} already exists.")
+            return
+
+        print(f"Downloading file {file_name}")
+
+        self.downloading_files.update({file_name : {}})
+        self.downloading_files[file_name].update({"Requested" : peers})
+        self.downloading_files[file_name].update({"Accepted" : []})
+        self.downloading_files[file_name].update({"Rejected" : []})
+        self.downloading_files[file_name].update({"Bytes" : 0})
+
+        for peer in peers:
+            t = ClientThread(peer, self)
+            t.requestFileDownload(file_name)
+
+    def startFileDownload(self, peers, file_name):
+        if len(peers) == 0:
+            print(f"File {file_name} donwload failed. Peers {' '.join(self.downloading_files[file_name]['Rejected'])} are not serving the file.")
+            self.downloading_files.pop(file_name)
+            return
+        
+        num_chunks = (self.downloading_files[file_name]["Bytes"] + CHUNK_SIZE - 1) // CHUNK_SIZE
+
+        self.downloading_files[file_name].update({"Chunks" : num_chunks})
+        self.downloading_files[file_name].update({"Chunks Received" : 0})
+
+        for i in range(num_chunks):
+            t = ClientThread(peers[i % len(peers)], self)
+            t.requestChunkDownload(file_name, i)
+        
+    def finishFileDownload(self, file_name):
+        if file_name not in self.downloading_files.keys():
+            print(f"File {file_name} download failed")
+            return
+
+        data = ""
+        for i in range(self.downloading_files[file_name]["Chunks"]):
+            data += self.downloading_files[file_name][i]
+        
+        with open("served_files/" + file_name, "w") as file:
+            file.write(data)
+
+        self.downloading_files.pop(file_name)
+
+        print(f"File {file_name} download success")
+
+    def requestFileUpload(self, peers, file_name):
+        if file_name not in getFileList():
+            print(f"Peer {MY_PEER_NAME} does not serve file {file_name}")
+            return
+        
+        print(f"Uploading file {file_name}")
+
+        for peer in peers:
+            t = ClientThread(peer, self)
+            t.requestFileUpload(file_name)
+
+
+    def run(self):
+        while True:
+            command = input().strip().split(' ')
+
+            request = command[0]
+
+            if request == "#FILELIST":
+                peers = command[1:]
+
+                self.requestFileList(peers)
+
+            if request == "#DOWNLOAD":
+                file_name = command[1]
+                peers = command[2:]
+
+                self.requestFileDownload(peers, file_name)
+
+            if request == "#UPLOAD":   
+
+                file_name = command[1]
+                peers = command[2:]
+
+                self.requestFileUpload(peers, file_name)
+
+
+
+class ClientThread(threading.Thread):
+    def __init__(self, peer, clientMain):
+        threading.Thread.__init__(self)
+        self.peer = peer   
+        self.clientMain = clientMain
+        self.response = ""        
+        self.peer_list = getPeerList()
+
+    def sendMessage(self, msg):
+        if len(msg) > 30:
+            print(f"Client ({MY_PEER_NAME}): {msg[0:30]}...")
+        else:
+            print(f"Client ({MY_PEER_NAME}): {msg}")
+
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            
+            if self.peer not in self.peer_list.keys():
+                raise socket.error
+            
+            serverIP, serverPort = self.peer_list[self.peer]
+            clientSocket.connect((serverIP, serverPort))
+            clientSocket.send(str(msg).encode())
+
+            self.response = ''
+            while self.response == '':
+                self.response = clientSocket.recv(MESSAGE_SIZE).decode()
+
+
+        except socket.error:
+            self.response = f"250 TCP connection to server {self.peer} failed"
+
+        finally:
+            clientSocket.close()
+
+    def getResponse(self):
+        while self.response == "":
+            continue
+
+        response = self.response
+        self.response = ""
+
+        if len(response) > 50:
+            print(f"Server ({self.peer}): {response[0:50]}...")
+        else:
+            print(f"Server ({self.peer}): {response}")
+
+        return response
+
+    def requestFileList(self):
+        peer = self.peer
+    
+        self.sendMessage("#FILELIST")
+        response = self.getResponse()
+
+    def requestFileDownload(self, file_name):
+        downloading_file = self.clientMain.downloading_files[file_name]
+        peer = self.peer
+
+        self.sendMessage(f"#DOWNLOAD {file_name}")
+        response = self.getResponse()
+        
+        status = response.split(' ')[0]
+
+        if status == "330":
+            downloading_file["Accepted"] += [peer]
+            downloading_file["Bytes"] = int(response.split(' ')[-1])
+
+        else:
+            downloading_file["Rejected"] += [peer]
+
+        if len(downloading_file["Accepted"]) + len(downloading_file["Rejected"]) == len(downloading_file["Requested"]):
+            self.clientMain.startFileDownload(downloading_file["Accepted"], file_name)
+
+    def requestChunkDownload(self, file_name, chunk_id):   
+        if file_name not in self.clientMain.downloading_files.keys():
+            return
+
+        downloading_file = self.clientMain.downloading_files[file_name]
+        downloading_file.update({chunk_id : None})
+
+        self.sendMessage(f"#DOWNLOAD {file_name} chunk {chunk_id}")
+        response = self.getResponse()
+
+        status = response.split(' ')[0]
+
+        if status == "200":
+            chunk = ' '.join(response.split(' ')[5:])
+
+            downloading_file[chunk_id] = chunk
+            downloading_file["Chunks Received"] += 1
+
+            if downloading_file["Chunks Received"] == downloading_file["Chunks"]:
+                self.clientMain.finishFileDownload(file_name)
+
+        else:
+            if file_name in self.clientMain.downloading_files.keys(): 
+                print(f"File {file_name} chunk {chunk_id} download failed")
+
+                self.clientMain.downloading_files.pop(file_name)
+
+
+    def requestFileUpload(self, file_name):
+        peer = self.peer
+
+        self.sendMessage(f"#UPLOAD {file_name} bytes {getBytesAmount(file_name)}")
+        response = self.getResponse()
+
+        status = response.split(' ')[0]
+
+        if status != "330":
+            return
+        
+        num_chunks = (getBytesAmount(file_name) + CHUNK_SIZE - 1) // CHUNK_SIZE
+
+        for chunk_id in range(num_chunks):
+            chunk = getChunk(file_name, chunk_id)
+
+            self.sendMessage(f"#UPLOAD {file_name} chunk {chunk_id} {chunk}")
+            response = self.getResponse()
+            
+            status = response.split(' ')[0]
+
+            if status != "200":
+                print(f"File {file_name} upload failed")
+                return
+
+        print(f"File {file_name} upload success")
+
+
+def main():
+    clientMain = ClientMain()
+    clientMain.run()
+
+if __name__ == '__main__':
+    args = sys.argv
+    MY_PEER_NAME = args[1]
+    main()
---
+++ server.py
@@ -0,0 +1,200 @@
+#!/usr/bin/python3
+
+import socket
+import threading
+import os
+import sys
+import hashlib
+
+CHUNK_SIZE = 100
+MESSAGE_SIZE = 256
+MY_PEER_NAME = "NO_NAME"
+
+def getFileList():
+    file_names = os.listdir("served_files")
+    files = [f for f in file_names if os.path.isfile(os.path.join("served_files", f)) and f.endswith(".txt")]
+    
+    print("Files: ", files)
+    return files
+
+def updateSettingsFile(id, ip, port):
+    peer_list = {}
+    with open("../peer_settings.txt", "r") as file:
+        for line in file.readlines():
+            peer_info = line.split()
+
+            peer_list.update({
+                peer_info[0] : (peer_info[1], peer_info[2])
+            })
+        
+    peer_list.update({
+        id : (ip, port)
+    })
+
+    with open("../peer_settings.txt", "w") as file:
+        for peer_id in peer_list.keys():
+            peer_ip, peer_port = peer_list[peer_id]
+            file.writelines(f"{peer_id} {peer_ip} {peer_port}\n")   
+
+
+def getChunk(file_name, chunk_id):
+    directory = "served_files/" + file_name
+
+    text = ""
+    with open(directory, "r") as file:
+        text = file.read()
+
+    ret = ""
+    for i in range(chunk_id * CHUNK_SIZE, min(len(text), (chunk_id + 1) * CHUNK_SIZE)):
+        ret += text[i]
+
+    return ret
+
+def getBytesAmount(file_name):
+    return int(os.path.getsize('served_files/' + file_name))
+
+class ServerMain:
+    def __init__(self, id):
+        self.id = id
+        hash_value = int(hashlib.md5(self.id.encode()).hexdigest(), 16)
+        port_range = 1000  # ports from 8000 to 8999
+        self.port = 8000 + (hash_value % port_range)
+        self.ip = socket.gethostname()
+        self.downloading_files = {}
+        updateSettingsFile(self.id, self.ip, self.port)
+    
+    
+    def serverRun(self):
+        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serverSocket.bind((self.ip, self.port))
+
+        IP = socket.gethostname()
+        serverSocket.listen()
+
+        while True:
+            client = serverSocket.accept()
+            t = ServerThread(client, self)
+            t.run()
+
+
+class ServerThread(threading.Thread):
+    def __init__(self, client, serverMain):
+        threading.Thread.__init__(self)
+        self.client = client
+        self.serverMain = serverMain
+        self.receiving_file = None
+
+    def parseRequest(self, request):
+        if len(request) > 50:
+            print(f"Client: {request[0:50]}...")
+        else:
+            print(f"Client: {request}")
+
+        command = request.split(' ')[0]
+
+        if command == "#FILELIST":
+            return "200 Files served: " + ' '.join(getFileList())
+            
+        if command == "#DOWNLOAD":
+            file_name = request.split(' ')[1]
+
+            if "chunk" not in request.split(' '):
+
+                if file_name not in getFileList():
+                    return f"250 Not serving file {file_name}"
+
+                else:
+                    return f"330 Ready to send file {file_name} bytes {getBytesAmount(file_name)}"
+
+            chunk_id = int(request.split(' ')[-1])
+        
+            return f"200 File {file_name} chunk {chunk_id} {getChunk(file_name, chunk_id)}"
+
+        if command == "#UPLOAD":
+
+            file_name = request.split(' ')[1]
+
+            if "chunk" not in request.split(' '):
+
+                if file_name in self.serverMain.downloading_files.keys():
+                    return f"250 Currently receiving file {file_name}"
+                
+                elif file_name in getFileList():
+                    return f"250 Already serving file {file_name}"
+
+                else:
+                    bytes = int(request.split(' ')[-1])
+                    num_chunks = (bytes + CHUNK_SIZE - 1) // CHUNK_SIZE
+
+                    print(f"Receiving file {file_name} bytes {bytes}")
+
+                    self.receiving_file = file_name
+
+                    self.serverMain.downloading_files.update({file_name : {}})
+                    self.serverMain.downloading_files[file_name].update({"Bytes" : bytes})
+                    self.serverMain.downloading_files[file_name].update({"Chunks" : num_chunks})
+                    self.serverMain.downloading_files[file_name].update({"Chunks Received" : 0})
+
+                    for chunk_id in range(num_chunks):
+                        self.serverMain.downloading_files[file_name].update({chunk_id : None})
+
+                    return f"330 Ready to receive file {file_name}"
+                
+            else:
+                chunk_id = int(request.split(' ')[3])
+                chunk = ' '.join(request.split(' ')[4:])
+
+                self.serverMain.downloading_files[file_name][chunk_id] = chunk
+
+                self.serverMain.downloading_files[file_name]["Chunks Received"] += 1
+
+                if self.serverMain.downloading_files[file_name]["Chunks Received"] == self.serverMain.downloading_files[file_name]["Chunks"]:
+                    
+                    data = ""
+                    for i in range(self.serverMain.downloading_files[file_name]["Chunks"]):
+                        data += self.serverMain.downloading_files[file_name][i]
+                    
+                    with open("served_files/" + file_name, "w") as file:
+                        file.write(data)
+
+                    self.serverMain.downloading_files.pop(file_name)
+                 
+                    return f"200 File {file_name} received"
+                else: 
+                    return f"200 File {file_name} chunk {chunk_id} received"
+
+
+
+    def run(self):
+        connectionSocket, addr = self.client
+        
+        try:
+            
+            request = ''
+            while request == '':
+                request = connectionSocket.recv(MESSAGE_SIZE).decode()
+
+            response = self.parseRequest(request)
+        
+        
+            print(f"Server ({MY_PEER_NAME}): ", response)
+
+
+            connectionSocket.send(response.encode())
+
+        except socket.error:   
+            print(f"TCP Connection to {addr} failed")
+
+            if self.receiving_file != None:
+                self.serverMain.downloading_files.pop(self.receiving_file)
+
+        finally:
+            connectionSocket.close()
+
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    MY_PEER_NAME = args[1]
+    server = ServerMain(MY_PEER_NAME)
+    server.serverRun() 
